a861022c25837bae3ab9be0470c39cd87a0e0db1
Ryo Nakae <ryo.n.1129@gmail.com>
2023-02-23 18:23:08 +0900

    update

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,10 @@
 import { EventHandler } from '@create-figma-plugin/utilities'
 
-export interface CreateRectanglesHandler extends EventHandler {
-  name: 'CREATE_RECTANGLES'
-  handler: (count: number) => void
+export interface ResizeWindowHandler extends EventHandler {
+  name: 'RESIZE_WINDOW'
+  handler: (windowSize: { width: number; height: number }) => void
 }
 
-export interface CloseHandler extends EventHandler {
-  name: 'CLOSE'
-  handler: () => void
+export type Settings = {
+  apiKey: string
 }
