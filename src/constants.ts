c2b859d01d88d155646ff003dd39f6dbb177e6aa
Ryo Nakae <ryo.n.1129@gmail.com>
2023-02-23 19:08:02 +0900

    save and load settings

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const SETTINGS_KEY = 'figma-gpt'
 export const UI_WIDTH = 400
-export const UI_HEIGHT = 300
+export const UI_HEIGHT = 0
---
+++ src/ui/App.tsx
@@ -0,0 +1,64 @@
+import { h, JSX } from 'preact'
+import { useEffect, useRef, useState } from 'preact/hooks'
+
+import { Tabs } from '@create-figma-plugin/ui'
+import { emit, once } from '@create-figma-plugin/utilities'
+
+import { UI_HEIGHT, UI_WIDTH } from '@/constants'
+import { ResizeWindowHandler, LoadSettingsHandler, Settings } from '@/types'
+import Setting from '@/ui/Setting'
+import Store from '@/ui/Store'
+
+export default function App() {
+  const { setApiKey } = Store.useContainer()
+  const [tabValue, setTabValue] = useState<null | string>(null)
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  const tabOptions = [
+    { children: <Setting />, value: 'Setting' },
+    { children: <div>Foo</div>, value: 'Chat' },
+    { children: <div>Bar</div>, value: 'Code' },
+  ]
+
+  function onTabChange(event: JSX.TargetedEvent<HTMLInputElement>) {
+    const newValue = event.currentTarget.value
+    setTabValue(newValue)
+  }
+
+  function resizeWindow() {
+    let height: number
+
+    if (wrapperRef.current) {
+      height = wrapperRef.current.clientHeight
+    } else {
+      height = UI_HEIGHT
+    }
+
+    emit<ResizeWindowHandler>('RESIZE_WINDOW', {
+      width: UI_WIDTH,
+      height,
+    })
+  }
+
+  function updateSettings(settings: Settings) {
+    setApiKey(settings.apiKey)
+  }
+
+  useEffect(() => {
+    setTabValue(tabOptions[0].value)
+
+    once<LoadSettingsHandler>('LOAD_SETTINGS', function (settings: Settings) {
+      updateSettings(settings)
+    })
+  }, [])
+
+  useEffect(() => {
+    resizeWindow()
+  }, [tabValue])
+
+  return (
+    <div ref={wrapperRef}>
+      <Tabs options={tabOptions} onChange={onTabChange} value={tabValue} />
+    </div>
+  )
+}
